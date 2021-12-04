31e4cddf9b54115503f9f60782888955351eb4ab
YurivanRuler <y.ruler@hotmail.com>
2021-12-04 20:52:55 +0100

    Added some logic with tests

--- src/app/movies/movies-list/movies-list.component.spec.ts
+++ src/app/movies/movies-list/movies-list.component.spec.ts
@@ -1,25 +1,42 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { Movie } from 'src/app/models/movie.model';
 import { MoviesListComponent } from './movies-list.component';
 
 describe('MoviesListComponent', () => {
-  let component: MoviesListComponent;
-  let fixture: ComponentFixture<MoviesListComponent>;
+  let sut: ComponentFixture<MoviesListComponent>;
+  let element: HTMLElement;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ MoviesListComponent ]
-    })
-    .compileComponents();
+      declarations: [MoviesListComponent],
+    }).compileComponents();
+
+    sut = TestBed.createComponent(MoviesListComponent);
+    element = sut.nativeElement;
   });
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(MoviesListComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+  it('Should list movies in a table', async () => {
+    // arrange
+    await setMovies([
+      { id: 1, title: 'Title', year: 2021, imdbTitleId: 'ImdbTitleId' },
+    ]);
+
+    // assert
+    expect(element.querySelectorAll('tbody tr').length).toEqual(1);
+    expect(element.querySelectorAll('div .alert')?.length).toEqual(0);
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('Should show alert when movies are undefined', async () => {
+    // arrange
+    await setMovies(undefined);
+    
+    // assert
+    expect(element.querySelector('div .alert')!.innerHTML!.trim()).toEqual('Oops! We can\'t find any movies!');
   });
+
+  async function setMovies(movies: Movie[] | undefined) {
+    sut.componentInstance.movies = movies;
+    sut.detectChanges();
+    await sut.whenStable();
+  }
 });
